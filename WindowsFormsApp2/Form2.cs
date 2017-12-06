89d830ce59f05569b51b3577335d62aef16b961e
Ayben <ayben.onal.ao@gmail.com>
2017-12-06 15:44:27 +0300

    Initial commit

---
+++ WindowsFormsApp2/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            { 
+                
+               
+                   double p = Convert.ToDouble(textBox1.Text);
+                if (p >= 0 && p<=0.75)
+                {
+                    double hesapla = (((-1.0) * (3.0 / 4.0)) * Math.Log((1.0 - ((4.0 / 3.0) * (double)p))));
+                    richTextBox1.SelectedText = Environment.NewLine + "K Değeri = " + hesapla.ToString();
+                    richTextBox1.SelectedText = Environment.NewLine + "Sonuç = İki Soyda " + Convert.ToInt32(hesapla*100) + " yer degiştirme vardı. Sonuçta " + (Convert.ToInt32(p * 100)) + " fark oluştu";
+                }
+                else
+                {
+                    MessageBox.Show("Girilen değer 0-0.75 arasında olmalı.");
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Veriler hatalı kontrol edin.");
+            }
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                double q = Convert.ToDouble(textBox1.Text);
+                double p = Convert.ToDouble(textBox2.Text);
+                if (q >= 0 && q <= 1 && p >= 0 && p <= 1)
+                {
+                    double hesapla = ((1.0 / 2.0) * Math.Log(1.0 / (1.0 - (2.0 * (double)p) - (double)q))) + ((1.0 / 4.0) * Math.Log(1.0 / (1.0 - (2.0 * (double)q))));
+                    richTextBox1.SelectedText = "K Değeri = " + hesapla.ToString();
+                    richTextBox1.SelectedText = Environment.NewLine + "Sonuç = İki Soyda " + Convert.ToInt32(hesapla * 100) + " yer degiştirme vardı. Sonuçta " + (Convert.ToInt32(p * 100 + q * 100)) + " fark oluştu";
+                }
+                else
+                {
+                    MessageBox.Show("Değerlerinizi kontrol ediniz. Transisyon=1 ise (0,01) yazınız. ");
+                }
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veriler hatalı kontrol edin.");
+            }
+        }
+    }
+}
